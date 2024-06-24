f38834d839d8ccc2f284a3610d5d777ba7321516
Hanan Ibrahim <ihanan977@gmail.com>
2024-06-24 22:37:51 +0300

    update assests and data files

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,5 +1,6 @@
 import LandPage from "../assets/projects/lnadmark-page.png";
 import Bloger from "../assets/projects/bloger.png";
+import Blog from "../assets/projects/blog.png";
 import CSS from "../assets/css.png";
 import HTML from "../assets/html.png";
 import JavaScript from "../assets/javascript.png";
@@ -8,41 +9,38 @@ import React from "../assets/react.png";
 export const data = [
   {
     id: 1,
-    title: "bookmark landing page react",
+    title: "Bookmark landing page react",
     desc: "The Bookmark Landing Page project is an innovative website built with React.js.  Its primary goal is to offer users a seamless and user-friendly platform for efficient bookmark management and organization. ",
     image: LandPage,
     live: "https://bookmark-landing-page-react-q826.vercel.app/",
   },
+
   {
     id: 2,
-    title: "Tesla home page clone ",
-    desc: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum laudantium eius dicta aliquid earum deserunt corporis molestias quo. Dolor magni veniam perferendis minima quo expedita consequatur ",
-    image: LandPage,
-    live: "https://www.realstar.ca/",
-  },
-  {
-    id: 3,
-    title: "Bloger",
+    title: "Bloger Landing Page",
     desc: "The Blogger Landing Page is focused on creating modren and responsive landing page for a blogging platform. The page was built using HTML, CSS and JavaScript.",
     image: Bloger,
     live: "https://bloger-landing-page-chi.vercel.app/",
   },
   {
+    id: 3,
+    title: "Blog App",
+    desc: "The Blog app built using ReactJS and firebase as backend to fetch data",
+    image: Blog,
+    live: "https://blog-app-i3i4.vercel.app/",
+  },
+  {
     id: 4,
-    title: "inspration",
+    title: "Tesla home page clone ",
     desc: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum laudantium eius dicta aliquid earum deserunt corporis molestias quo. Dolor magni veniam perferendis minima quo expedita consequatur ",
     image: LandPage,
-    live: "",
+    live: "https://www.realstar.ca/",
   },
 ];
 
 export const skillsData = [
   { id: 1, title: "HTML", icon: HTML },
   { id: 2, title: "CSS", icon: CSS },
-  {
-    id: 3,
-    title: "JavaScript",
-    icon: JavaScript,
-  },
+  { id: 3, title: "JavaScript", icon: JavaScript },
   { id: 4, title: "React", icon: React },
 ];
